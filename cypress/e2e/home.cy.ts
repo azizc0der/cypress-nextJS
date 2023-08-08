47655b758f2d1de11df6132327c34f11fe6a64ad
aziz <asaydraxmonov15@gmail.com>
2023-08-08 23:58:56 +0500

    initial commit

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -12,6 +12,6 @@ describe("home page", () => {
    it("the features on the homepage are correct", () => {
       cy.get("dt").eq(0).contains("4 Courses")
       cy.get("dt").eq(1).contains("25+ Lessons")
-      cy.get("dt").eq(2).contains("Free and Open Source")
+      cy.get("dt").eq(2).contains("Free and Open Source");
    })
 })
