9774673bfe5de41ea23532b67a868cf493fe9af7
aziz <asaydraxmonov15@gmail.com>
2023-08-08 23:56:56 +0500

    initial commit

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,9 @@
+// describe("Newsletter Subscribe Form", () => {
+//    beforeEach(() => {
+//       cy.visit('http://localhost:3001')
+//    })
+//
+//    it('allows users to subscribe to the email list', () => {
+//       cy.getByData('email-input').type("tom@aol.com")
+//    })
+// })
